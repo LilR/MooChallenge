a02d8538b3ee078f72334ef294a46176eeea1951
LilR <lilyroberts95@gmail.com>
2019-06-30 14:12:13 +0100

    Initial commit for pom and runnables

---
+++ src/main/java/com/lilyjordan/moochallenge/MooChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lilyjordan.moochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooChallengeApplication.class, args);
+	}
+
+}
